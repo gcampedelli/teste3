a6ccd76ebbbf87654027cbb9ee2a228ad04d01bd
gcampedelli <gcampedelli@users.noreply.github.com>
2014-04-19 11:48:08 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,23 +1,14 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import requests 
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+html = requests.get("https://www.secom.planalto.gov.br/consea/boletins.nsf/01ContatoxNome?OpenView&Start=1", verify=False).content
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+import lxml.html
+root = lxml.html.fromstring(html)
+for table in root.cssselect('table')[1]:
+    data={
+    'nome': table[0].text_content(),
+    'email': table[2].text_content(),
+        }
+
+print data
