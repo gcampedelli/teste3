5a5ada00d197403614aec367c863b7248f302960
gcampedelli <gcampedelli@users.noreply.github.com>
2014-04-19 15:34:25 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,9 +5,9 @@ html = requests.get("https://www.secom.planalto.gov.br/consea/boletins.nsf/01Con
 
 import lxml.html
 root = lxml.html.fromstring(html)
-table=root.cssselect('table')[1]:
-        for td in table:
-                data={
+table=root.cssselect('table')[1]
+for td in table:
+        data={
                 'nome': table[0].text_content(),
                 'email': table[2].text_content(),
         }
